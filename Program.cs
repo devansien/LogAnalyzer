3ff768c74a4856b80b3f422270690ea25ad9e55b
devansien <devansien@gmail.com>
2019-05-29 22:01:34 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<string> categories = new List<string>();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Log Analyzer
+
+This project consists of:
+* A log analyzer and preprocessor.
+
+## Description:
+
+```
+    1. Sample text
+```
+
+```
+    2. Sample text
+```
+
+```
+    3. Sample text
+```
